134d4d03ba4cd6f1cb7995c83c323e975fa85ac1
Fabrício T.Sandri <fabriciosandri9@gmail.com>
2026-09-23 18:31:43 -0300

    tp2

---
+++ TPs/tp2/TP02Q01 - Modelagem em Java/Modelagem.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+class Veiculo{
+    private int id;
+    private String marca;
+    private String modelo;
+    private int ano;
+    private String categoria;
+    private String combustivel;
+    private int cilindros;
+    private double cilindrada;
+    private String transmissao;
+    private String tracao;
+    private double consumoCidade;
+    private double consumoEstrada;
+    private double consumo;
+    private boolean turbo;
+    private Data dataRegistro;
+}
+class Data{}
+class Fabricio{
+static public void main(String[] args){}
+}
+
+
